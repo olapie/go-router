62d8bf241981bc44c86b2cd53795d14bd429d004
yxz6 <yxzbox@gmail.com>
2022-12-03 12:55:18 -0500

    Add Handler Func

--- handler.go
+++ handler.go
@@ -3,8 +3,6 @@ package router
 import (
 	"container/list"
 	"context"
-
-	"code.olapie.com/errors"
 )
 
 type HandlerWrapper[H any] list.Element
@@ -22,8 +20,20 @@ type Handler[IN any, OUT any] interface {
 	Handle(ctx context.Context, in IN) OUT
 }
 
+type HandlerFunc[IN any, OUT any] func(ctx context.Context, in IN) OUT
+
+func (f HandlerFunc[IN, OUT]) Handle(ctx context.Context, in IN) OUT {
+	return f(ctx, in)
+}
+
 type HandlerWithError[IN any, OUT any] interface {
 	Handle(ctx context.Context, in IN) (OUT, error)
+}
+
+type HandlerFuncWithError[IN any, OUT any] func(ctx context.Context, in IN) (OUT, error)
+
+func (f HandlerFuncWithError[IN, OUT]) Handle(ctx context.Context, in IN) (OUT, error) {
+	return f(ctx, in)
 }
 
 type contextKey int
@@ -33,7 +43,11 @@ const keyNextHandler contextKey = iota
 func Next[IN any, OUT any](ctx context.Context, in IN) (out OUT) {
 	h, ok := ctx.Value(keyNextHandler).(*HandlerWrapper[Handler[IN, OUT]])
 	if !ok {
-		panic("cannot find next handler")
+		f, ok := ctx.Value(keyNextHandler).(*HandlerWrapper[HandlerFunc[IN, OUT]])
+		if !ok {
+			panic("cannot find next handler")
+		}
+		h = (*HandlerWrapper[Handler[IN, OUT]])(f)
 	}
 
 	// if h.Next() is nil, then next call of NextWithError will stop
@@ -44,8 +58,11 @@ func Next[IN any, OUT any](ctx context.Context, in IN) (out OUT) {
 func NextWithError[IN any, OUT any](ctx context.Context, in IN) (out OUT, err error) {
 	h, ok := ctx.Value(keyNextHandler).(*HandlerWrapper[HandlerWithError[IN, OUT]])
 	if !ok {
-		err = errors.NotImplemented("cannot find next handler")
-		return
+		f, ok := ctx.Value(keyNextHandler).(*HandlerWrapper[HandlerFuncWithError[IN, OUT]])
+		if !ok {
+			panic("cannot find next handler")
+		}
+		h = (*HandlerWrapper[HandlerWithError[IN, OUT]])(f)
 	}
 
 	// if h.Next() is nil, then next call of NextWithError will stop
